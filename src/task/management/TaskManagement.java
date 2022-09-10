cfa2018ab6f58c3af133f6e795bf4212050f5d44
OliJoel0411 <106788709+OliJoel0411@users.noreply.github.com>
2022-09-10 00:18:37 -0600

    Primer commit

---
+++ src/task/management/TaskManagement.java
@@ -0,0 +1,14 @@
+
+package task.management;
+
+/**
+ * @author Oliver
+ */
+public class TaskManagement {
+
+    public static void main(String[] args) {
+        // Proyecto UMG
+        System.out.println("Task Management...");
+    }
+    
+}
