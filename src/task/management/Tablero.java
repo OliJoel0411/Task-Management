8c5a4b4567270c4736ac02c71409b7a7279c0f68
OliJoel0411 <106788709+OliJoel0411@users.noreply.github.com>
2022-10-11 16:01:06 -0600

    Agregando un tablero

--- src/task/management/Tablero.java
+++ src/task/management/Tablero.java
@@ -5,5 +5,34 @@ package task.management;
  * @author Oliver
  */
 public class Tablero {
+    // Atributos de cada tablero
+    private String nombre;
+    private int id;
+
+    // Constructo
+    public Tablero(String nombre, int id) {
+        this.nombre = nombre;
+        this.id = id;
+    }
+    
+    // Método consultor
+    public String getNombre() {
+        return nombre;
+    }
+    //Método modificador
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    // Método consultor
+    public int getId() {
+        return id;
+    }
+    // Método modificador
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+    
     
 }
--- src/task/management/TaskManagement.java
+++ src/task/management/TaskManagement.java
@@ -1,19 +1,42 @@
 
 package task.management;
 
+import java.util.Scanner;
+
 /**
  * @author Oliver
  */
 public class TaskManagement {
 
     public static void main(String[] args) {
-        // Proyecto UMG
-        System.out.println("Task Management...");
-        System.out.println("Mi primer cambio en Git");
-        System.out.println("Tercer Commit");
-        /*
-        haciendo cambios
-        */
+      // Variables globales
+      String opc = " ",nombre = " ";
+      int id = 0;
+      Scanner tc = new Scanner(System.in);
+      // Creando un objeto de tipo Tablero
+      Tablero tab1 = new Tablero(nombre, id);
+            
+      
+      
+        System.out.println("Desea crear un Tablero");
+        System.out.println("s/n: ");
+        opc = tc.next();
+        
+        if(opc.equals("s")){
+            System.out.print("Escibar el nombre del Tablero: ");
+            nombre = tc.next();
+            tab1.setNombre(nombre);
+            
+            id = 1;   
+            tab1.setId(id);
+        }
+        
+        System.out.println("Identificacin del tablero creado");
+        System.out.println("Nombre: " + tab1.getNombre());
+        System.out.println("Id: " + tab1.getId());
+        
+      
+      
     }
     
 }
